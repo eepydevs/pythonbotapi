4a5ecdcbf77407d6fe13343a11cd578140a405cc
Number1 <72150634+1randomguyspecial@users.noreply.github.com>
2023-03-07 18:34:45 +0300

    some bug fixes and minor improvements

--- api.py
+++ api.py
@@ -2,7 +2,7 @@ import os
 import datetime, time
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
-from utils import RedisManager
+from utils import db
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -22,22 +22,19 @@ def main():
     def get(self):
       query_parameters = request.args
       userid = query_parameters.get("userid")
-      
-      with RedisManager(host = os.environ["REDISHOST"], port = os.environ["REDISPORT"], password = os.environ["REDISPASSWORD"], client_name = os.environ["REDISUSER"]) as db:
-        if userid:
-          return db["balance"][str(userid)] if str(userid) in db["balance"] else ({"message": "The user could not be found.", "status": 404}, 404)
-        else:
-          return db["balance"]
+      if userid:
+        return db["balance"][str(userid)] if str(userid) in db["balance"] else ({"message": "The user could not be found.", "status": 404}, 404)
+      else:
+        return db["balance"]
         
   class Inventory(Resource):
     def get(self):
       query_parameters = request.args
       userid = query_parameters.get("userid")
-      with RedisManager(host = os.environ["REDISHOST"], port = os.environ["REDISPORT"], password = os.environ["REDISPASSWORD"], client_name = os.environ["REDISUSER"]) as db:
-        if userid:
-          return db["inventory"][str(userid)] if str(userid) in db["inventory"] else ({"message": "The user could not be found.", "status": 404}, 404)
-        else:
-          return db["inventory"]
+      if userid:
+        return db["inventory"][str(userid)] if str(userid) in db["inventory"] else ({"message": "The user could not be found.", "status": 404}, 404)
+      else:
+        return db["inventory"]
 
   api.add_resource(Balance, "/api/balance")
   api.add_resource(Inventory, "/api/inventory")
