cddcd78abd848d746bd569e66d1bf9f227f1a4fb
Number1 <72150634+1randomguyspecial@users.noreply.github.com>
2023-03-22 17:57:07 +0300

    some updates idk

--- utils.py
+++ utils.py
@@ -1,30 +1,54 @@
 import redis as rd
+import atexit
 import json
 import os
 
-def ping(redis: rd.Redis):
-  success = 0
-  try:
-      redis.ping()
-      success = 1
-  except (rd.exceptions.AuthenticationError, rd.exceptions.ConnectionError) as err:
-      pass
-  return success
+class JsonFile(object):
+  __data: Dict[Any, Any]
+  def __init__(self, filename):
+      self.filename = filename
+      self.__data = {}
+      self.load()
 
-class RedisConnectionError(Exception):
-    pass
+  def __enter__(self):
+      return self.__data
+
+  def __exit__(self, exc_type, exc_val, exc_tb):
+      self.save()
+
+  def __contains__(self, item):
+      return item in self.__data
+
+  def load(self):
+      try:
+          with open(self.filename, 'r') as f:
+              self.__data = json.load(f)
+      except (FileNotFoundError, json.decoder.JSONDecodeError):
+          with open(self.filename, 'w') as f:
+              json.dump({}, f)
+
+  def save(self):
+      with open(self.filename, "w") as f:
+          json.dump(self.__data, f, indent = 4)
+
+  @property
+  def data(self):
+      return self.__data
+
+  @data.setter
+  def data(self, value):
+      self.__data = value
 
 class RedisDB(dict):
   def __init__(self, name: str = "main", key: str = None, *, host: str, port: int, password: str, client_name: str, charset: str = "utf-8", decode_responses: bool = True):
       self._redis = rd.Redis(host = host, port = port, password = password, client_name = client_name, charset = charset, decode_responses = decode_responses)
       super().__init__()
+      self._backup = JsonFile("backup.json")
       self._name = name
       self._key = key if key else name
-      self._var = self._load()
+      self._var = self._load(True)
       atexit.register(self.__del)
-      self.ping_success = ping(self._redis)
-      if not self.ping_success:
-          raise RedisConnectionError("Unable to connect to Redis. Use Replit as backup.")
+      self._redis.close()
 
   def __getitem__(self, key):
       return self._var[key]
@@ -44,19 +68,27 @@ class RedisDB(dict):
   def __contains__(self, item):
       return item in self._var
 
-  def _load(self):
+  def _load(self, backup = False):
       if self._key not in self._redis.keys():
           self._redis.hset(self._name, self._name, "{}")
       self._var = json.loads(self._redis.hget(self._name, self._key))
+      if backup:
+          if self._var != self._backup.data and self._backup.data:
+              self._var = self._backup.data
+              self._backup.data = {}
+              self._backup.save()
       return self._var
 
   def _save(self):
-      if self._var != json.loads(self._redis.hget(self._name, self._key)):
-          self._redis.hset(self._name, self._key, json.dumps(self._var))
+      try:
+          if self._var != json.loads(self._redis.hget(self._name, self._key)):
+              self._redis.hset(self._name, self._key, json.dumps(self._var))
+      except:
+          self._backup.data = self._var
+          self._backup.save()
 
   def __del(self):
-      if self.ping_success:
-          self._save()
+      self._save()
 
 class Singleton(type):
   _instances = {}
@@ -80,3 +112,5 @@ class RedisManager(metaclass = Singleton):
   def __exit__(self, exc_type, exc_value, exc_traceback):
     if self._var != json.loads(self._redis.hget(self._name, self._key)):
       self._redis.hset(self._name, self._key, json.dumps(self._var))
+
+db = RedisDB(host = os.environ["REDISHOST"], port = os.environ["REDISPORT"], password = os.environ["REDISPASSWORD"], client_name = os.environ["REDISUSER"])
