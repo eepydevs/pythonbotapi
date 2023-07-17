6b5b139e6c300e364cbc20317879b72ecdeeaa62
Number1 <72150634+1randomguyspecial@users.noreply.github.com>
2023-07-17 17:39:57 +0300

    vercel

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 import os
 import datetime, time
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, url_for
 from flask_restful import Resource, Api
 from utils import db
 from dotenv import load_dotenv
@@ -13,15 +13,18 @@ def main():
   @app.errorhandler(404)
   def page_not_found(e):
       return "<h1>404</h1><p>The resource could not be found.</p>", 404
-    
+
   @app.route("/")
   def home():
-    return "<center><h1>Home page</h1><center><p>The available endpoints:<br>/api/ping<br>/api/balance (userid=discord_id optional)<br>/api/inventory (userid=discord_id optional)</p>"
+    return """<center><h1>Home page</h1><center><p>Available endpoints:<br>/api/ping<br>/api/balance (userid=discord_id optional)<br>/api/bank (userid=discord_id optional) (IN_DEV)<br>/api/inventory (userid=discord_id optional)</p>"""
 
   class Ping(Resource):
     def get(self):
-      return db["ping"]
-  
+      try:
+        return db["ping"]
+      except Exception as e:
+        return -1
+
   class Balance(Resource):
     def get(self):
       query_parameters = request.args
@@ -30,7 +33,16 @@ def main():
         return db["balance"][str(userid)] if str(userid) in db["balance"] else ({"message": "The user could not be found.", "status": 404}, 404)
       else:
         return db["balance"]
-        
+
+  class Bank(Resource):
+    def get(self):
+      query_parameters = request.args
+      userid = query_parameters.get("userid")
+      if userid:
+        return db["bank"][str(userid)] if str(userid) in db["bank"] else ({"message": "The user could not be found.", "status": 404}, 404)
+      else:
+        return db["bank"]
+
   class Inventory(Resource):
     def get(self):
       query_parameters = request.args
@@ -42,8 +54,10 @@ def main():
 
   api.add_resource(Ping, "/api/ping")
   api.add_resource(Balance, "/api/balance")
+  api.add_resource(Bank, "/api/bank")
   api.add_resource(Inventory, "/api/inventory")
-  
+  #api.add_url_rule("/favicon.ico", redirect_to = url_for("static", filename = "favicon.ico"))
+
   app.run("0.0.0.0", os.environ["PORT"])
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+from typing import Dict, Any
 import redis as rd
 import atexit
 import json
@@ -40,21 +41,23 @@ class JsonFile(object):
       self.__data = value
 
 class RedisDB(dict):
-  def __init__(self, name: str = "main", key: str = None, *, host: str, port: int, password: str, client_name: str, charset: str = "utf-8", decode_responses: bool = True):
-      self._redis = rd.Redis(host = host, port = port, password = password, client_name = client_name, charset = charset, decode_responses = decode_responses)
-      super().__init__()
-      self._backup = JsonFile("backup.json")
-      self._name = name
-      self._key = key if key else name
-      self._var = self._load(True)
-      atexit.register(self.__del)
-      self._redis.close()
+  def __init__(self, name: str = "main", key: str = None, *, host: str, port: int, password: str, client_name: str, charset: str = "utf-8", decode_responses: bool = True, dont_save: bool = False):
+    self._redis = rd.Redis(host = host, port = port, password = password, client_name = client_name, charset = charset, decode_responses = decode_responses, health_check_interval = 1000)
+    super().__init__()
+    self._backup = JsonFile("backup.json")
+    self._name = name
+    self.__ds = dont_save
+    self._key = key if key else name
+    self._var = self._load(True)
+    atexit.register(self.__del)
 
   def __getitem__(self, key):
       return self._var[key]
 
   def __setitem__(self, key, value):
       self._var[key] = value
+      if not self.__ds:
+        self._save()
 
   def __repr__(self):
       return self._var.__repr__()
@@ -63,7 +66,8 @@ class RedisDB(dict):
       return self._load()
 
   def __exit__(self, exc_type, exc_value, exc_traceback):
-      self._save()
+      if not self.__ds:
+        self._save()
 
   def __contains__(self, item):
       return item in self._var
@@ -88,7 +92,8 @@ class RedisDB(dict):
           self._backup.save()
 
   def __del(self):
-      self._save()
+      if not self.__ds:
+          self._save()
 
 class Singleton(type):
   _instances = {}
@@ -96,21 +101,72 @@ class Singleton(type):
     if cls not in cls._instances:
       cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
     return cls._instances[cls]
-    
+
 class RedisManager(metaclass = Singleton):
   def __init__(self, name: str = "main", key: str = None, *, host: str, port: int, password: str, client_name: str, charset: str = "utf-8", decode_responses: bool = True):
     self._redis = rd.Redis(host = host, port = port, password = password, client_name = client_name, charset = charset, decode_responses = decode_responses)
     self._name = name
     self._key = key if key else name
-  
+
   def __enter__(self) -> dict:
     if self._key not in self._redis.keys():
       self._redis.hset(self._name, self._name, "{}")
     self._var = json.loads(self._redis.hget(self._name, self._key))
     return self._var
-  
+
   def __exit__(self, exc_type, exc_value, exc_traceback):
-    if self._var != json.loads(self._redis.hget(self._name, self._key)):
+    pass
+
+class RedisDBLive(dict):
+  def __init__(self, name: str = "main", key: str = None, *, host: str, port: int, password: str, client_name: str, charset: str = "utf-8", decode_responses: bool = True, dont_save: bool = False):
+    super().__init__()
+    self._redis = rd.Redis(host = host, port = port, password = password, client_name = client_name, charset = charset, decode_responses = decode_responses, health_check_interval = 1000)
+    atexit.register(self.__del)
+    self._backup = JsonFile("backup.json")
+    self.__ds = dont_save
+    self._name = name
+    self._var = {}
+    self._key = key if key else name
+    self.__load()
+
+  def __getitem__(self, item):
+    # if self._var == {}:
+    #   self._var = json.loads(self._redis.hget(self._name, self._key))
+    return self._var[item]
+
+  def __setitem__(self, key, value):
+    self._var[key] = value
+    if not self.__ds:
       self._redis.hset(self._name, self._key, json.dumps(self._var))
 
-db = RedisDB(host = os.environ["REDISHOST"], port = os.environ["REDISPORT"], password = os.environ["REDISPASSWORD"], client_name = os.environ["REDISUSER"])
+  def __contains__(self, item):
+    # if self._var == {}:
+    #   self._var = json.loads(self._redis.hget(self._name, self._key))
+    return item in self._var
+
+  def __repr__(self):
+    return self._var.__repr__()
+
+  def __del(self):
+    if not self.__ds:
+      try:
+        self._redis.hset(self._name, self._key, json.dumps(self._var))
+      except Exception as e:
+        print(e)
+        self._var["crashed"] = True
+        self._backup.data = self._var
+        self._backup.save()
+    self._redis.close()
+
+  def __load(self):
+    if self._var == {}:
+      if "crashed" in self._backup.data:
+        if self._backup.data["crashed"]:
+          del self._backup.data["crashed"]
+          self._var = self._backup.data
+        else:
+          self._var = json.loads(self._redis.hget(self._name, self._key))
+      else:
+        self._var = json.loads(self._redis.hget(self._name, self._key))
+
+db = RedisDBLive(host = os.environ["REDISHOST"], port = os.environ["REDISPORT"], password = os.environ["REDISPASSWORD"], client_name = None, dont_save = True)
