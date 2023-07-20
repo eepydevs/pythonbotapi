13204bccf46c3ef4be26b721d8d079df07bdaf71
Number1 <72150634+1randomguyspecial@users.noreply.github.com>
2023-07-20 20:45:00 +0300

    vercel mwork

--- api.py
+++ api.py
@@ -1,64 +1,59 @@
 import os
 import datetime, time
-from flask import Flask, jsonify, request, url_for
-from flask_restful import Resource, Api
+from flask import Flask, request
 from utils import db
 from dotenv import load_dotenv
 load_dotenv()
 
-def main():
-  app = Flask(__name__)
-  api = Api(app)
 
-  @app.errorhandler(404)
-  def page_not_found(e):
-      return "<h1>404</h1><p>The resource could not be found.</p>", 404
+app = Flask(__name__)
 
-  @app.route("/")
-  def home():
-    return """<center><h1>Home page</h1><center><p>Available endpoints:<br>/api/ping<br>/api/balance (userid=discord_id optional)<br>/api/bank (userid=discord_id optional) (IN_DEV)<br>/api/inventory (userid=discord_id optional)</p>"""
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<h1>404</h1><p>The resource could not be found.</p>", 404
 
-  class Ping(Resource):
-    def get(self):
-      try:
-        return db["ping"]
-      except Exception as e:
-        return -1
+@app.route('/favicon.ico')
+def favicon():
+    return app.send_static_file('favicon.ico')
 
-  class Balance(Resource):
-    def get(self):
-      query_parameters = request.args
-      userid = query_parameters.get("userid")
-      if userid:
-        return db["balance"][str(userid)] if str(userid) in db["balance"] else ({"message": "The user could not be found.", "status": 404}, 404)
-      else:
-        return db["balance"]
+@app.route("/")
+def home():
+  return """<center><h1>Home page of python bot</h1><center><p>Available urls: /api</p>"""
 
-  class Bank(Resource):
-    def get(self):
-      query_parameters = request.args
-      userid = query_parameters.get("userid")
-      if userid:
-        return db["bank"][str(userid)] if str(userid) in db["bank"] else ({"message": "The user could not be found.", "status": 404}, 404)
-      else:
-        return db["bank"]
+@app.route("/api")
+def api():
+  return """<center><h1>Available endpoints:</h1><center><p>/api/ping<br>/api/balance (userid=discord_id optional)<br>/api/bank (userid=discord_id optional) (IN_DEV)<br>/api/inventory (userid=discord_id optional)</p>"""
 
-  class Inventory(Resource):
-    def get(self):
-      query_parameters = request.args
-      userid = query_parameters.get("userid")
-      if userid:
-        return db["inventory"][str(userid)] if str(userid) in db["inventory"] else ({"message": "The user could not be found.", "status": 404}, 404)
-      else:
-        return db["inventory"]
+@app.route("/api/ping")
+def ping():
+  try:
+    return {"ping": db["ping"]}
+  except Exception as e:
+    return {"ping": -1}
 
-  api.add_resource(Ping, "/api/ping")
-  api.add_resource(Balance, "/api/balance")
-  api.add_resource(Bank, "/api/bank")
-  api.add_resource(Inventory, "/api/inventory")
-  #api.add_url_rule("/favicon.ico", redirect_to = url_for("static", filename = "favicon.ico"))
+@app.route("/api/balance")
+def balance():
+  query_parameters = request.args
+  userid = query_parameters.get("userid")
+  if userid:
+    return {"balance": db["balance"][str(userid)]}
+  else:
+    return db["balance"]
 
-  app.run("0.0.0.0", os.environ["PORT"])
+@app.route("/api/bank")
+def bank():
+  query_parameters = request.args
+  userid = query_parameters.get("userid")
+  if userid:
+    return {"bank": db["bank"][str(userid)]} if str(userid) in db["bank"] else ({"message": "The user could not be found.", "status": 404}, 404)
+  else:
+    return db["bank"]
 
-if __name__ == "__main__":
-  main()
+@app.route("/api/inventory")
+def inventory():
+  query_parameters = request.args
+  userid = query_parameters.get("userid")
+  if userid:
+    return db["inventory"][str(userid)] if str(userid) in db["inventory"] else ({"message": "The user could not be found.", "status": 404}, 404)
+  else:
+    return db["inventory"]
